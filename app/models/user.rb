a118245c7517e87954f9bed4a208adaffac710ba
Matthew Cataldi & Nico Glennon <pair+mjcataldi.nicoglennon@devbootcamp.com>
2017-06-29 17:24:33 -0500

    Added password functionality and helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :questions, foreign_key: :author_id
   has_many :answers, foreign_key: :author_id
   has_many :comments, foreign_key: :commenter_id
   has_many :votes, foreign_key: :voter_id
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
