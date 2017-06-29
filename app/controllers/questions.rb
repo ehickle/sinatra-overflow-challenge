237c05e0961cf4fa8e7ed0695387034a8beedb85
Evan Hickle & Nico Glennon <pair+ehickle.nicoglennon@devbootcamp.com>
2017-06-29 18:01:47 -0500

    Add button to home page that redirects to form for creating a new question.

---
+++ app/controllers/questions.rb
@@ -0,0 +1,17 @@
+get '/questions' do
+  erb :'/questions/index'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  # redirect '/' unless current_user
+  @author = User.create(username: 'tim', email: 'tim@tom.com', password_hash: 'timtom')
+
+  @author.questions.create(params[:question])
+  # @question = Question.create(params[:question])
+  # @author = User.find(1)
+  redirect '/'
+end
