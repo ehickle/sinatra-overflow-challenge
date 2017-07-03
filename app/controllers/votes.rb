6760dd64430668f0dd8fbf69a22325db94ce64ce
James W Moody <jamesmoodyradio@gmail.com>
2017-07-02 19:52:37 -0500

    Add extra voting features and styling

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -4,3 +4,9 @@ post '/answers/:id/votes' do
   question_id = @answer.question.id
   redirect "/questions/#{question_id}"
 end
+
+post '/questions/:id/votes' do
+  @question = Question.find(params[:id])
+  @question.votes.create(value: params[:vote], voter_id: current_user.id)
+  redirect "/questions/#{ @question.id }"
+end
