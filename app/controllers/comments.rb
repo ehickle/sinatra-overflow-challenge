35ff4b6090e266d249e2682970a280b803e6cc03
Nico Glennon <nicoglennon@icloud.com>
2017-07-07 12:16:48 -0500

    Ajaxify all comments

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,23 +3,43 @@
 # get/display form for new comment on a question
 get "/questions/:question_id/comments/new" do
   @question = Question.find(params[:question_id])
-  @new_comment_on_question = true
-  erb :"questions/show"
+  if request.xhr?
+    # some stuff
+    erb :"questions/_comment_form", layout: false
+  else
+    @new_comment_on_question = true
+    erb :"questions/show"
+  end
 end
 
 # post/persist a comment on a question
 post '/questions/:question_id/comments' do
-  comment = Comment.new(body:params[:body])
-  @question = Question.find(params[:question_id])
-
-  comment.commentable = Question.find(params[:question_id])
-  comment.commenter = current_user
-
-  if comment.save
-    erb :"/questions/show"
+    comment             = Comment.new(body:params[:body])
+    @question           = Question.find(params[:question_id])
+    comment.commentable = Question.find(params[:question_id])
+    comment.commenter   = current_user
+  if request.xhr?
+    if comment.save
+      content_type :json
+        { 
+          id: comment.id, 
+          body: comment.body, 
+          time_since_creation: comment.time_since_creation, 
+          commenter: comment.commenter.username,
+          commenter_id: comment.commenter.id
+        }.to_json
+    else
+      status 422
+    end
   else
-    @errors = ["Comment cannot be blank"]
-    erb :"/questions/show"
+
+
+    if comment.save
+      erb :"/questions/show"
+    else
+      @errors = ["Comment cannot be blank"]
+      erb :"/questions/show"
+    end
   end
 end
 
@@ -27,12 +47,21 @@ end
 # TODO: make a DELETE form button instead, for RESTfulness
 get '/comments/:comment_id/delete' do
   comment = Comment.find(params[:comment_id])
-  comment.destroy!
 
-  if comment.commentable.class == Question
-    redirect "/questions/#{comment.commentable.id}"
+  if request.xhr?
+    if comment.destroy!
+      status 200
+    else
+      status 422
+    end
+
   else
-    redirect "/questions/#{comment.commentable.question.id}"
+    comment.destroy!
+    if comment.commentable.class == Question
+      redirect "/questions/#{comment.commentable.id}"
+    else
+      redirect "/questions/#{comment.commentable.question.id}"
+    end
   end
 end
 
@@ -41,20 +70,24 @@ end
 
 # get/display form for new comment on an answer
 get "/questions/:question_id/answers/:answer_id/comments/new" do
-  @question = Question.find(params[:question_id])
-  @answer = Answer.find(params[:answer_id])
+  @question              = Question.find(params[:question_id])
+  @answer                = Answer.find(params[:answer_id])
   @new_comment_on_answer = @answer.id
-  erb :"questions/show"
+  if request.xhr?
+    erb :"questions/_comment_form", layout: false
+  else
+    erb :"questions/show"
+  end
 end
 
 # post/persist a comment on an answer
 post '/questions/:question_id/answers/:answer_id/comments' do
-  comment = Comment.new(body:params[:body])
+  comment   = Comment.new(body:params[:body])
   @question = Question.find(params[:question_id])
-  @answer = Answer.find(params[:answer_id])
-
+  @answer   = Answer.find(params[:answer_id])
+  
   comment.commentable = @answer
-  comment.commenter = current_user
+  comment.commenter   = current_user
 
   if comment.save
     erb :"/questions/show"
